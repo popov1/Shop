938803ef5b445f82d3c202ce39e80661314d12d0
popov1 <popov.evgeniy1@mail.ru>
2017-04-18 20:07:27 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+<div class="Name" align = "center"  >
+<div class="container" >
+
+  <footer>
+    © 2017 Popov. Все права защищены.
+  </footer>
+</div>
+</div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,19 @@
+<?php 
+//header.php
+?>
+<!DOCTYPE html>
+	<html lang="rus">
+	<head>
+	<meta charset="utf-8"> 
+	<meta name="viewport" content="width=device-width">
+	<link href="css_mag/style.css" rel="stylesheet" type="text/css">	
+	<title>Интернет магазин SHINA 24.*.ua</title>
+	</head>
+	<body>
+
+	<div class="Name_header" align = "center"  >
+		<h1>САЙТ SHINA 24.*.ua</h1>
+		<div class="container" >
+		<?php include ("Menu_Mag.php"); ?>
+		</div>
+	</div>
